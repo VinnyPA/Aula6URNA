46aa3853ae2b8c8e403f8161862bdaba3dba7820
Aluno <Aluno@LAB06-29>
2022-09-26 21:37:43 -0300

    Corrigido e Finalizadocorrigir

--- Aula6/Form1.cs
+++ Aula6/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,7 +20,8 @@ namespace Aula6
         string numero = "";
         Candidato alguem;
         Candidato[] lista = new Candidato[4];
-        int branco = 0, nulo = 0;
+        int eleitores = 0, branco = 0, nulo = 0;
+        
         
         private void insereCandidato()
         {
@@ -63,52 +65,136 @@ namespace Aula6
 
         private void btn5_Click(object sender, EventArgs e)
         {
+            tecla();
             Preenche("5");
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
+            tecla();
             Preenche("2");
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
+            tecla();
             Preenche("3");
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
+            tecla();
             Preenche("6");
         }
 
         private void btn1_Click(object sender, EventArgs e)
         {
+            tecla();
             Preenche("1");
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
+            tecla();
             Preenche("4");
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
+            tecla();
             Preenche("7");
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
+            tecla();
             Preenche("8");
         }
 
         private void btn0_Click(object sender, EventArgs e)
         {
+            tecla();
             Preenche("0");
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
+            tecla();
             Preenche("9");
+        }
+
+        private void tecla()
+        {
+            SoundPlayer som = new SoundPlayer(@"C:\Users\Aluno\Desktop\musicas\tecla.wav");
+            som.Play();
+        }
+        private void btnConfirma_Click(object sender, EventArgs e)
+        {
+            SoundPlayer som = new SoundPlayer(@"C:\Users\Aluno\Desktop\musicas\urna.wav");
+            som.Play();
+            int valido = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (numero == "Branco")
+                {
+                    branco++;
+                    valido = 1;
+                }
+                else
+                {
+                    if (int.Parse(numero) == lista[i].Numero)
+                    {
+                        lista[i].Voto++;
+                        valido = 1;
+                    }
+                }
+            }
+            
+                if (valido == 0)
+                {
+                    nulo++;
+                }
+            
+
+            eleitores++;
+            corrige();
+            if (eleitores == 9)
+            {
+                string texto = "Nulo =" + nulo + "\nBranco= " + branco;
+                for (int i = 0; i < 4; i++)
+                {
+                    texto += "\n" + lista[i].Nome + "=" + lista[i].Voto;
+                }
+                MessageBox.Show(texto);
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            tecla();
+            numero = "Branco";
+            lblMensagem.Visible = true;
+            btnConfirma.Enabled = true;
+            lblCandidato.Text = "VOTO EM BRANCO";
+            
+
+        }
+        private void corrige()
+        {
+            txtNum1.Text = null;
+            txtNum2.Text = null;
+            lblCandidato.Text = null;
+            lblTurma.Text = null;
+            lblMensagem.Visible = false;
+            btnConfirma.Enabled = false;
+            numero="";
+            pxFoto.Image  = null;
+
+        }
+        
+        private void button12_Click(object sender, EventArgs e)
+        {
+            corrige();
         }
 
         private void Preenche(string n)
